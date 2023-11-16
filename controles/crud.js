3bdcb424629e65ea1287a00321e9ae34fb9a632d
Orlando Najera <flammer.terminal@gmail.com>
2023-11-15 20:09:56 -0600

    edit and delete created

--- controles/crud.js
+++ controles/crud.js
@@ -2,12 +2,28 @@ const conexion = require('../database/db');
 
 exports.save = (req, res) => {
     const titulo = req.body.titulo;
-    const nombre = req.body.nombre;
-    const apellidos = req.body.apellidos;
     const paginas = req.body.paginas;
     const fecha_publicacion = req.body.fecha_publicacion;
     const editorial = req.body.editorial;
-    conexion.query('INSERT INTO libro SET ?', { titulo: titulo, paginas: paginas, fecha_publicacion: fecha_publicacion, editorial: editorial }, 'INSERT INTO autor SET ?', { nombre: nombre, apellidos: apellidos }, (error, results) => {
+    const nombre = req.body.nombre;
+    const apellidos = req.body.apellidos;
+    conexion.query('INSERT INTO libro SET ?', { titulo: titulo, paginas: paginas, fecha_publicacion: fecha_publicacion, editorial: editorial }, (error, results) => {
+        if (error) {
+            console.error(error);
+        } else {
+            res.redirect('/');
+        }
+    })
+}
+
+exports.update = (req, res) => {
+    const titulo = req.body.titulo;
+    const paginas = req.body.paginas;
+    const fecha_publicacion = req.body.fecha_publicacion;
+    const editorial = req.body.editorial;
+    const nombre = req.body.nombre;
+    const apellidos = req.body.apellidos;
+    conexion.query('UPDATE libro SET ? WHERE id = ?', [{ titulo: titulo, paginas: paginas, fecha_publicacion: fecha_publicacion, editorial: editorial }, { id: id }], (error, results) => {
         if (error) {
             console.error(error);
         } else {
--- router.js
+++ router.js
@@ -19,7 +19,32 @@ router.get('/create', (req, res) => {
     res.render('create');
 });
 
+//EDITAR REGISTROS
+router.get('/edit/:id', (req, res) => {
+    const id = req.params.id;
+    conexion.query('SELECT * FROM libro WHERE id=?', [id], (error, results) => {
+        if (error) {
+            return console.log('error:' + error);
+        } else {
+            res.render('edit', { libro: results[0] });
+        };
+    })
+});
+
+//ELIMINAR REGISTRO
+router.get('/delete/:id', (req, res) => {
+    const id = req.params.id;
+    conexion.query('DELETE FROM users WHERE id = ?', [id], (error, resultados) => {
+        if (error) {
+            return console.log('error:' + error);
+        } else {
+            res.redirect('/');
+        };
+    })
+})
+
 const crud = require('./controles/crud');
 router.post('/save', crud.save);
+router.post('/update', crud.update);
 
 module.exports = router;
